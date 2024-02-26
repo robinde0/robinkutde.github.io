5fad5d1f26c88b2c867bd5d0e8daac5bd36cb4e1
Robin <robkut999@gmail.com>
2024-02-26 18:08:50 +0100

    Update 1
    update 1

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Maintenance Mode</title>
+    <link rel="stylesheet" href="style.css">
+    <meta property="og:title" content="File Upload | robinde" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="http://debug.robinde.de" />
+    <meta property="og:url" content="http://debug.robinde.tech" />
+    <meta property="og:description" content="Dies ist meine Debug Seite. (Only Insiders)" />
+    <meta name="theme-color" content="#0091ff">
+</head>
+<body>
+    <h1>Debug Page</h1>
+
+</body>
+</html>
